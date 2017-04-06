41d5a38534f3d30458c5ab68cea02a6f53aaf95a
Luis H. P. Mendes <luishpmendes@gmail.com>
2017-04-06 15:35:39 -0300

    arrumando utilidade incremental

--- src/grasp.cpp
+++ src/grasp.cpp
@@ -15,35 +15,48 @@ typedef pair < vector <uint>, double > tSolution;
 
 tSolution greedyRandomizedConstruction (matrix A, vector <double> incrementalUtility, double alpha, default_random_engine generator) {
     tSolution result = make_pair(vector <uint> (A.size(), 0), 0.0);
+    
+    double minUtility = numeric_limits <double> :: max(), maxUtility = numeric_limits <double> :: min();
 
     while (true) {
-        double minUtility = numeric_limits <double> :: max(), maxUtility = numeric_limits <double> :: min();
-        vector < pair <ulint, double> > candidateList;
         for (ulint i = 0; i < A.size(); i++) {
             if (result.first[i] == 0 && (i <= 0 || result.first[i - 1] == 0) && (i >= A.size() - 1 || result.first[i + 1] == 0)) { // if canditade can be put in solution
-                if (incrementalUtility[i] > 0) { // if this candidate can improve solution
-                    candidateList.push_back(make_pair(i, incrementalUtility[i]));
-                    if (minUtility > incrementalUtility[i]) {
-                        minUtility = incrementalUtility[i];
+                if ((*incrementalUtility)[i] > 0) { // if this candidate can improve solution
+                    if (minUtility > (*incrementalUtility)[i]) {
+                        minUtility = (*incrementalUtility)[i];
                     }
-                    if (maxUtility < incrementalUtility[i]) {
-                        maxUtility = incrementalUtility[i];
+                    if (maxUtility < (*incrementalUtility)[i]) {
+                        maxUtility = (*incrementalUtility)[i];
                     }
                 }
             }
         }
         double restriction = maxUtility - alpha * (maxUtility - minUtility);
         vector < pair <ulint, double> > restrictedCandidateList;
-        for (ulint i = 0; i < candidateList.size(); i++) {
-            if (candidateList[i].second >= restriction) {
-                restrictedCandidateList.push_back(candidateList[i]);
+        for (ulint i = 0; i < A.size(); i++) {
+            if ((*incrementalUtility)[i] >= restriction) {
+                restrictedCandidateList.push_back(make_pair(i, (*incrementalUtility)[i]));
             }
         }
         if (restrictedCandidateList.size() > 0) {
             uniform_int_distribution <ulint> distribution (0, restrictedCandidateList.size() - 1);
             ulint s = distribution(generator);
-            result.first[restrictedCandidateList[s].first] = 1;
-            result.second += restrictedCandidateList[s].second;
+            ulint k = restrictedCandidateList[s].first;
+            ulint deltaUtility = restrictedCandidateList[s].second;
+            result.first[k] = 1;
+            result.second += deltaUtility;
+            // atualizando utilidade incremental
+            (*incrementalUtility)[k] += result.first[k] * A[k][k] * result.first[k];
+            for (ulint i = 0; i < A.size(); i++) {
+                if (i != k) {
+                    (*incrementalUtility)[i] += result.first[i] * A[i][k] * result.first[k];
+                }
+            }
+            for (ulint j = 0; j < A.size(); j++) {
+                if (j != k) {
+                    (*incrementalUtility)[j] += result.first[k] * A[k][j] * result.first[j];
+                }
+            }
         } else {
             break;
         }
@@ -66,7 +79,7 @@ bool isFeasible(tSolution solution) {
     return true;
 }
 
-bool oneFlip (matrix A, vector <double> incrementalUtility, default_random_engine generator, tSolution * solution) {
+bool oneFlip (matrix A, vector <double> * incrementalUtility, default_random_engine generator, tSolution * solution) {
     bool result = false;
     vector <ulint> neighborhood;
     for (ulint i = 0; i < A.size(); i++) {
@@ -77,27 +90,53 @@ bool oneFlip (matrix A, vector <double> incrementalUtility, default_random_engin
         flag = false;
         shuffle (neighborhood.begin(), neighborhood.end(), generator);
         for (vector <ulint> :: iterator it = neighborhood.begin(); it != neighborhood.end(); it++) {
-            ulint i = *it;
+            ulint k = *it;
             tSolution newSolution = make_pair(vector <ulint> ((*solution).first), (*solution).second);
-            if (newSolution.first[i] == 0) {
-                newSolution.first[i] = 1;
-                newSolution.second += incrementalUtility[i];
+            if (newSolution.first[k] == 0) {
+                newSolution.first[k] = 1;
+                newSolution.second += (*incrementalUtility)[k];
             } else {
-                newSolution.first[i] = 0;
-                newSolution.second -= incrementalUtility[i];
+                newSolution.first[k] = 0;
+                newSolution.second -= (*incrementalUtility)[k];
             }
             if (isFeasible(newSolution) && newSolution.second >= (*solution).second) {
                 result = true;
                 flag = true;
                 (*solution).first = vector <ulint> (newSolution.first);
                 (*solution).second = newSolution.second;
+                // atualizando utilidade incremental
+                if ((*solution).first[k] == 1) {
+                    (*incrementalUtility)[k] += A[k][k];
+                    for (ulint i = 0; i < A.size(); i++) {
+                        if (i != k) {
+                            (*incrementalUtility)[i] += result.first[i] * A[i][k];
+                        }
+                    }
+                    for (ulint j = 0; j < A.size(); j++) {
+                        if (j != k) {
+                            (*incrementalUtility)[j] += A[k][j] * result.first[j];
+                        }
+                    }
+                } else {
+                    (*incrementalUtility)[k] -= A[k][k];
+                    for (ulint i = 0; i < A.size(); i++) {
+                        if (i != k) {
+                            (*incrementalUtility)[i] -= result.first[i] * A[i][k];
+                        }
+                    }
+                    for (ulint j = 0; j < A.size(); j++) {
+                        if (j != k) {
+                            (*incrementalUtility)[j] -= A[k][j] * result.first[j];
+                        }
+                    }
+                }
             }
         }
     }
     return result;
 }
 
-bool twoFlip (matrix A, vector <double> incrementalUtility, default_random_engine generator, tSolution * solution) {
+bool twoFlip (matrix A, vector <double> * incrementalUtility, default_random_engine generator, tSolution * solution) {
     bool result = false;
     vector < vector <ulint> > neighborhood;
     for (ulint i = 0; i < A.size(); i++) {
@@ -127,13 +166,14 @@ bool twoFlip (matrix A, vector <double> incrementalUtility, default_random_engin
                 flag = true;
                 (*solution).first = vector <ulint> (newSolution.first);
                 (*solution).second = newSolution.second;
+                // atualizar utilidade incremental
             }
         }
     }
     return result;
 }
 
-void localSearch (matrix A, vector <double> incrementalUtility, default_random_engine generator, tSolution * solution) {
+void localSearch (matrix A, vector <double> * incrementalUtility, default_random_engine generator, tSolution * solution) {
 
 }
 
@@ -146,10 +186,10 @@ bool termination (chrono :: high_resolution_clock :: time_point tBegin, ulint ti
     return false;
 }
 
-
-
-void repair(matrix A, tSolution * solution) {
-
+void repair(matrix A, vector <double> * incrementalUtility, tSolution * solution) {
+    // desligar bits até se tornar factível
+    // dar preferencia pros bits que violam mais restricões
+    // em caso de empate, dar preferencia pros que diminuem menos a utilidade
 }
 
 tSolution grasp (matrix A, chrono :: high_resolution_clock :: time_point tBegin, ulint timeLimit, double alpha, ulint seed) {
@@ -158,23 +198,13 @@ tSolution grasp (matrix A, chrono :: high_resolution_clock :: time_point tBegin,
     default_random_engine generator (seed);
 
     vector <double> incrementalUtility (A.size(), 0.0);
-    for (ulint i = 0; i < A.size(); i++) {
-        for (ulint j = 0; j < A[i].size(); j++) {
-            incrementalUtility[i] += A[i][j];
-            incrementalUtility[j] += A[i][j];
-        }
-    }
-    for (ulint i = 0; i < A.size(); i++) {
-        incrementalUtility[i] -= A[i][i];
-    }
-
     bool flag = true;
     while (termination (tBegin, timeLimit) != true) {
-        tSolution solution = greedyRandomizedConstruction(A, incrementalUtility, alpha, generator);
-        localSearch(A, incrementalUtility, generator, &solution);
+        tSolution solution = greedyRandomizedConstruction(A, &incrementalUtility, alpha, generator);
+        localSearch(A, &incrementalUtility, generator, &solution);
 
         if (!isFeasible(solution)) {
-            repair(A, &solution);
+            repair(A, &incrementalUtility, &solution);
         }
 
         if (flag || result.second > solution.second) {
